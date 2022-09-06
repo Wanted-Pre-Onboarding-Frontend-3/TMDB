f49ab3802c641e8a478a95dcca68eed88b4ad0fa
Lihu Kim <lh.ines.kim@gmail.com>
2022-09-06 21:19:11 +0900

    feat: Home 기본 구조 셋팅
    Header와 Footer의 기본 틀 구현

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,3 +1,31 @@
-export default function Home() {
-  return <div>메인 페이지</div>;
-}
+import Footer from 'components/Footer';
+import Header from 'components/Header';
+import styled from 'styled-components';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+
+      <Main>
+        <Container></Container>
+      </Main>
+
+      <Footer />
+    </div>
+  );
+};
+
+const Main = styled.main`
+  max-width: 1280px;
+  margin: 0 auto;
+  border: 1px solid red;
+`;
+
+const Container = styled.div`
+  margin-top: 62px;
+  padding: 24px 0;
+  border: 1px solid red;
+`;
+
+export default Home;
