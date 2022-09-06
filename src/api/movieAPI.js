0467e173bb50e784404bd57300952fcdbe52b752
ycs1m1yk <satoly4@gmail.com>
2022-09-06 22:29:46 +0900

    Feat: movie API 작성
    '/movie/*' 경로를 이용하는 요청들 모음

---
+++ src/api/movieAPI.js
@@ -0,0 +1,26 @@
+import { axiosInstance } from 'api/axios-instance';
+
+const movieAPI = {
+  getPopularMovies: async ({ params }) => {
+    const { data } = await axiosInstance.get('/movie/popular', { params });
+    return data;
+  },
+  getNowPlayingMovies: async ({ params }) => {
+    const { data } = await axiosInstance.get('/movie/now_playing', { params });
+    return data;
+  },
+  getUpcomingMovies: async ({ params }) => {
+    const { data } = await axiosInstance.get('/movie/upcoming', { params });
+    return data;
+  },
+  getTopRatedMovies: async ({ params }) => {
+    const { data } = await axiosInstance.get('/movie/top_rated', { params });
+    return data;
+  },
+  getMovieById: async ({ movie_id, params }) => {
+    const { data } = await axiosInstance.get(`/movie/${movie_id}`, { params });
+    return data;
+  },
+};
+
+export default movieAPI;
