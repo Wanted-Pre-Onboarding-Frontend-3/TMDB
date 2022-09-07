7d63f81b8d29cfd127856798ce5941d65ed76b3f
ycs1m1yk <satoly4@gmail.com>
2022-09-07 16:03:59 +0900

    chore: prettier 포매팅

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,6 @@ import { colors } from 'styles/colors';
 import { fonts } from 'styles/fonts';
 
 const Header = () => {
-
   // TODO: 메뉴 링크 경로 확인, 검색기능 구현
 
   const [searchKeyword, setSearchKeyword] = useState('');
